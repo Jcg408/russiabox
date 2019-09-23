0057fab243eee5ce4ff659fb28bc93887d64ac87
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-09-23 09:42:00 -0400

    add routes folder to backend for boxes and notes

---
+++ backend/routes/notes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+let Note = requires('../models/note');
+
+router.route('/').get((req, res) => {
+    Note.find()
+    .then(notes => res.json(notes))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/add').post((req, res)=> {
+    const subject = req.body.subject;
+    const content = req.body.content;
+
+    newNote = new Note({subject, content});
+
+    newNote.save()
+    .then(()=> res.json('Note added!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+})
+
+module.exports = router;
